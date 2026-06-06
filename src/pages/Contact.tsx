52355f6b49f53a09f0dfe6826961b9bf1968b0a0
vidarbhabana-web <vidarbhabana@gmail.com>
2026-06-06 14:50:27 +0530

    Add placeholder for Contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Contact: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Contact page will be implemented with Contact.tsx */}
+    </div>
+  )
+}
+
+export default Contact
